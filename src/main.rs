f2e4730055f50b95b5549a9b02ac26a68051b2f5
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-01 13:39:56 +0300

    📦 chore(Cargo.toml): Add dependencies serde and thiserror 🔧 fix(main.rs): Handle file opening and JSON parsing errors
    In the `Cargo.toml` file, the following dependencies were added:
    - `serde` version 1.0.163 with the `derive` feature
    - `serde_json` version 1.0.96
    - `thiserror` version 1.0.40
    
    In the `src/main.rs` file, the code was updated as follows:
    - Imported necessary dependencies: `serde::{Deserialize, Serialize}`, `std::fs::File`, `std::io::BufReader`, and `thiserror::Error`
    - Defined custom error types using `thiserror::Error` for better error handling
    - Created structs `Surah` and `Verse` with the corresponding field types
    - Modified the `main` function to return a `Result` and added a `QuranError` enum to handle errors
    - Added error handling for file opening and JSON parsing using `File::open`, `BufReader::new`, and `serde_json::from_reader`
    - Printed the length of the `quran` vector to verify successful parsing
    
    Changes made:
    - Added dependencies `serde` and `serde_json` to the `Cargo.toml` file
    - Imported necessary dependencies in `src/main.rs`
    - Defined custom error types using `thiserror::Error`
    - Added error handling for file opening and JSON parsing
    - Printed the length of the `quran` vector for verification
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::BufReader;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum QuranError {
+    #[error("failed to open file: {0}")]
+    FileOpenError(String),
+    #[error("failed to parse JSON: {0}")]
+    JsonError(String),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Surah {
+    id: u32,
+    name: String,
+    transliteration: String,
+    translation: String,
+    #[serde(rename = "type")]
+    type_: String,
+    total_verses: u32,
+    verses: Vec<Verse>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Verse {
+    id: u32,
+    text: String,
+    translation: String,
+}
+
+fn main() -> Result<(), QuranError> {
+    let file_path = "quran.json";
+    let file = File::open(file_path)
+        .map_err(|e| QuranError::FileOpenError(format!("{}: {}", file_path, e)))?;
+    let reader = BufReader::new(file);
+
+    let quran: Vec<Surah> = serde_json::from_reader(reader)
+        .map_err(|e| QuranError::JsonError(format!("{}: {}", file_path, e)))?;
+
+    println!("{:?}", quran.len());
+
+    Ok(())
 }
