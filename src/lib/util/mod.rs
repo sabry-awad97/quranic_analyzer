6437b0769e1bb744943ebdf517ddab953cdf410d
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-01 17:59:30 +0300

    ✨ feat(util): Add tashkeel stripping function The commit adds a new utility function `strip_tashkeel` to remove tashkeel marks from Arabic text. Tashkeel marks are diacritical marks used in Arabic script to represent short vowels and other phonetic features. The function iterates over the characters of the input text, checking if each character is a tashkeel mark. If it is not a tashkeel mark, the character is appended to the `stripped` string. The resulting stripped text is returned.
    Additionally, three helper functions are included:
    - `is_vocalized` checks if a word contains any tashkeel marks.
    - `is_tashkeel` determines whether a character is a tashkeel mark.
    
    Unit tests have been added to ensure the correctness of the functions.
    
    Changes made:
    - Added `strip_tashkeel` function to strip tashkeel marks from Arabic text.
    - Included helper functions `is_vocalized` and `is_tashkeel` to support `strip_tashkeel`.
    - Added unit tests for the new functions.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

---
+++ src/lib/util/mod.rs
@@ -0,0 +1,78 @@
+const FATHATAN: char = '\u{064B}';
+const DAMMATAN: char = '\u{064C}';
+const KASRATAN: char = '\u{064D}';
+const FATHA: char = '\u{064E}';
+const DAMMA: char = '\u{064F}';
+const KASRA: char = '\u{0650}';
+const SHADDA: char = '\u{0651}';
+const SUKUN: char = '\u{0652}';
+
+const TASHKEEL: [char; 8] = [
+    FATHATAN, DAMMATAN, KASRATAN, FATHA, DAMMA, KASRA, SUKUN, SHADDA,
+];
+
+pub fn strip_tashkeel(text: &str) -> String {
+    let mut stripped = String::new();
+    for c in text.chars() {
+        if !is_tashkeel(c) {
+            stripped.push(c);
+        }
+    }
+    stripped
+}
+
+#[allow(unused)]
+fn is_vocalized(word: &str) -> bool {
+    word.chars().any(|c| is_tashkeel(c))
+}
+
+fn is_tashkeel(archar: char) -> bool {
+    TASHKEEL.contains(&archar)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_strip_tashkeel() {
+        // Test case with mixed text and tashkeel marks
+        let text = "الْعَرَبِيّةُ";
+        let stripped = strip_tashkeel(text);
+        assert_eq!(stripped, "العربية");
+
+        // Test case with only tashkeel marks
+        let text = "َُِّ";
+        let stripped = strip_tashkeel(text);
+        assert_eq!(stripped, "");
+
+        // Test case with no tashkeel marks
+        let text = "Hello World!";
+        let stripped = strip_tashkeel(text);
+        assert_eq!(stripped, "Hello World!");
+    }
+
+    #[test]
+    fn test_is_vocalized() {
+        // Test case with vocalized word
+        let word = "مَرْحَبًا";
+        let vocalized = is_vocalized(word);
+        assert_eq!(vocalized, true);
+
+        // Test case with non-vocalized word
+        let word = "سلام";
+        let vocalized = is_vocalized(word);
+        assert_eq!(vocalized, false);
+    }
+
+    #[test]
+    fn test_is_tashkeel() {
+        // Test case with tashkeel mark
+        let tashkeel = 'َ';
+        assert_eq!(is_tashkeel(tashkeel), true);
+
+        // Test case with non-tashkeel character
+        let non_tashkeel = 'م';
+        assert_eq!(is_tashkeel(non_tashkeel), false);
+    }
+}
