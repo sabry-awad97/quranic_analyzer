239cefd07a0b28dbd6250c1e0222fecd3303f144
Sabry Awad <dr.sabry1997@gmail.com>
2023-06-01 19:19:11 +0300

    🔄 refactor(main.rs): Refactor main function and remove unused code The main function in `main.rs` has been refactored to remove unused code and simplify the logic. The `Options` struct and command-line arguments are no longer used. Additionally, the `Analyzer` and `QuranSearch` instances are no longer created or used.
    Changes made:
    - Removed the `Options` struct and related command-line arguments.
    - Removed the creation of `Analyzer` and `QuranSearch` instances.
    - Removed the code related to printing various statistics about the Quran.
    - Updated the output to print the length of the concordance.
    
    This refactoring improves code clarity and removes unnecessary complexity.
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,13 @@
-use quranic_analyzer::quran::{analyze::Analyzer, search::QuranSearch, Quran};
-
-use structopt::StructOpt;
-
-#[derive(StructOpt)]
-struct Options {
-    /// Print the number of total letters in the Quran.
-    #[structopt(short, long)]
-    total_letters: bool,
-
-    /// Print the number of words in the Quran.
-    #[structopt(short, long)]
-    words: bool,
-
-    /// Print the longest chapter in the Quran.
-    #[structopt(short, long)]
-    longest_surah: bool,
-
-    /// Print the shortest chapter in the Quran.
-    #[structopt(short, long)]
-    shortest_surah: bool,
-
-    /// Print the most common word in the Quran.
-    #[structopt(short, long)]
-    most_common_word: bool,
-}
+use quranic_analyzer::quran::Quran;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let options = Options::from_args();
     let mut quran = Quran::new()?;
     let concordance = quran.generate_concordance();
 
     for (word, count) in concordance {
         println!("The word {} appears {} times in the Quran.", word, count);
     }
-    
-    let analyzer = Analyzer::new(&quran);
-    let summary = analyzer.analyze();
 
-    if options.total_letters {
-        println!("The Holy Quran has {} letters.", summary.total_letters);
-    }
-
-    if options.words {
-        println!("The Holy Quran has {} words.", summary.total_words);
-    }
-
-    if options.longest_surah {
-        println!(
-            "The longest surah is {} with {} letters.",
-            summary.longest_surah_name, summary.longest_surah_letters
-        );
-    }
-
-    if options.shortest_surah {
-        println!(
-            "The shortest chapter is {} with {} letters.",
-            summary.shortest_surah_name, summary.shortest_surah_letters
-        );
-    }
-
-    if options.most_common_word {
-        if let Some((word, count)) = summary.most_common_word {
-            println!(
-                "The most common word is {} with {} occurrences.",
-                word, count
-            );
-        }
-    }
-
-    let mut quran_search = QuranSearch::new(&quran);
-    let search_results = quran_search.search("الله");
-
-    println!("Search Results:");
-    if search_results.is_empty() {
-        println!("No matching results found.");
-    } else {
-        for (surah_name, ayah_text) in &search_results {
-            println!("Surah: {}", surah_name);
-            println!("Ayah: {}", ayah_text);
-            println!("---");
-        }
-    }
-    println!("{}", search_results.len());
+    println!("The concordance length is {}", concordance.len());
     Ok(())
 }
